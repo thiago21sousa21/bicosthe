e6d2344eeabef89aeeb66dd3ccd9fff4bcba48ee
thiago <thiago@thiago.com>
2025-05-22 14:55:20 -0300

    inciando rotas de pagina

--- bicosthe-front/src/App.js
+++ bicosthe-front/src/App.js
@@ -1,9 +1,32 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 
+function Home() {
+  return <h1>Página Inicial</h1>;
+}
+
+function Sobre() {
+  return <h1>Sobre nós</h1>;
+}
+
+function Contato() {
+  return <h1>Fale conosco</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Início</Link> | 
+        <Link to="/sobre">Sobre</Link> | 
+        <Link to="/contato">Contato</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
+    </Router>
   );
 }
 
