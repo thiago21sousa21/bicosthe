00dc2fbd0f4ec40ae5726b935f7ba36bd94743bf
thiago21sousa21 <thiago21sousa21@gmail.com>
2025-06-09 10:35:59 -0300

    aplicando validação de schema em rotas de usuario

--- bicosthe-api/src/routes/users.routes.js
+++ bicosthe-api/src/routes/users.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { controllers } from "../controllers/index.js";
+import {schemaCadastroUsuario, schemaLogin} from "../schemas/index.js"
+import {schemaValidation} from "../middlewares/schema.validation.js"
 
 const route = Router();
-route.post('/signup',/* schemaValidation(signupSchema),*/ controllers.user.register);
-route.post('/signin',/* s schemaValidation(signinSchema),*/  controllers.user.login);
+route.post('/signup', schemaValidation(schemaCadastroUsuario), controllers.user.register);
+route.post('/signin',schemaValidation(schemaLogin), controllers.user.login);
 
 export const users = {route};
